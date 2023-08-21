3b465be17f30a11477cdcdda8714d2e09577b428
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-08-21 21:54:15 +0300

    ss

--- app/components/SecondaryBanner/SecondaryBanner.jsx
+++ app/components/SecondaryBanner/SecondaryBanner.jsx
@@ -13,22 +13,20 @@ const SecondaryBanner = () => {
          <Container maxWidth="xl">
            <Grid container sx={{paddingTop : '100px', flexDirection : {xs : 'column-reverse', md : 'row'}}} justifyContent={'center'} alignItems={'center'}>
            <Grid item xs={12} md={6}>
-               <LazyLoad height={"100%"} once>
-                  <Typography variant='h1' sx={{fontSize : '40px', width : '90%', color : '#312f3a', fontWeight : 'bold'}}>
-                    ماسيه الشرق لاستقدام و الحاق العمالة بالخارج
-                    </Typography>
-                    <Typography variant='body1' sx={{color : '#312f3a', fontSize : '1.1rem', lineHeight : 2.3,  width : '90%', marginTop : '20px'}}>
-                    هي علامة تجارية رائدة لحلول الخدمات المنزلية بالسعودية ، تقدم ماسية الشرق حلولًا مبتكرة للقطاع
-                    المنزلي من خلال توظيف عمالة منزلية مدربة على أعلى مستوى.
+                <Typography variant='h1' sx={{fontSize : '40px', width : '90%', color : '#312f3a', fontWeight : 'bold'}}>
+                ماسيه الشرق لاستقدام و الحاق العمالة بالخارج
+                </Typography>
+                <Typography variant='body1' sx={{color : '#312f3a', fontSize : '1.1rem', lineHeight : 2.3,  width : '90%', marginTop : '20px'}}>
+                هي علامة تجارية رائدة لحلول الخدمات المنزلية بالسعودية ، تقدم ماسية الشرق حلولًا مبتكرة للقطاع
+                المنزلي من خلال توظيف عمالة منزلية مدربة على أعلى مستوى.
 
-                    </Typography>
-                  <Box sx={{ margin : '40px 0px'}}>
-                      <Image loading="lazy" src={playStoreImage} alt="ماسيه الشرق لاستقدام و الحاق العمالة بالخارج" style={{margin : '0px 20px', cursor : 'pointer'}}/>
-                      <Image loading="lazy" src={appStoreImage} alt="ماسيه الشرق لاستقدام و الحاق العمالة بالخارج" style={{margin : '0px', cursor : 'pointer',}}/>
-                  </Box>
-               </LazyLoad>
+                </Typography>
+              <Box sx={{ margin : '40px 0px'}}>
+                  <Image loading="lazy" src={playStoreImage} alt="ماسيه الشرق لاستقدام و الحاق العمالة بالخارج" style={{margin : '0px 20px', cursor : 'pointer'}}/>
+                  <Image loading="lazy" src={appStoreImage} alt="ماسيه الشرق لاستقدام و الحاق العمالة بالخارج" style={{margin : '0px', cursor : 'pointer',}}/>
+              </Box>
             </Grid>
-            <Grid item xs={12} md={5} sx={{marginBottom : {xs : '15px' , md : 0}, }} >
+            <Grid item xs={12} md={5} sx={{marginBottom : {xs : '15px' , md : 0}, display : {xs : 'none', md : "block"}}} >
              <Image priority={true} src={personalImage} alt='ماسيه الشرق لاستقدام و الحاق العمالة بالخارج' layout='responsive'/>
             </Grid>
             <Grid item></Grid>
